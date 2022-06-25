3a1dbc7f62e3b3282293ec0adaaea24923aa7b1f
Johneryy <johnakintolu4@gmail.com>
2022-06-26 00:38:38 +0800

    practicals tho

---
+++ src/OOP/Car.java
@@ -0,0 +1,16 @@
+package OOP;
+
+public class Car {
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2022;
+    String color = "Blue";
+    double price = 12_000_000.00;
+
+    void drive() {
+        System.out.println("You drive the car");
+    }
+    void brake() {
+        System.out.println("You stop the car");
+    }
+}
