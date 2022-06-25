19f3b7d2d3675101b34d91c43bdea5957cd9c4bf
Johneryy <johnakintolu4@gmail.com>
2022-06-26 01:19:26 +0800

    practicals tho

--- src/ToStringMethod/Car.java
+++ src/ToStringMethod/Car.java
@@ -4,4 +4,8 @@ public class Car {
     String name = "Corolla";
     String color = "White";
     int year = 2022;
+
+    public String toString() {
+        return name+"\n"+color+"\n"+year+"\n";
+    }
 }
