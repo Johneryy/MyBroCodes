cb7a4704f0ea3d14795bf3d862f0449cace45e8f
Johneryy <johnakintolu4@gmail.com>
2022-06-26 00:51:56 +0800

    practicals tho

--- src/OOP/Main2.java
+++ src/OOP/Main2.java
@@ -4,5 +4,8 @@ public class Main2 {
     public static void main(String[] args) {
         Human human = new Human("Johnery",19,75);
         System.out.println(human.name);
+
+        human.eat();
+
     }
 }
