dca42cfb4e23994c3e70fe896535fde94fe58aa5
Johneryy <johnakintolu4@gmail.com>
2022-06-26 01:13:08 +0800

    practicals tho

--- src/ToStringMethod/toString.java
+++ src/ToStringMethod/toString.java
@@ -2,7 +2,9 @@ package ToStringMethod;
 
 public class toString {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        System.out.println("The name of the car is "+car.name);
+        System.out.println("The color of the car is "+car.color);
+        System.out.println("The year of the car is "+car.year);
     }
-
 }
