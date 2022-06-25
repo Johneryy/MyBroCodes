89a517537124fc19ddd054ca028042ac46ec37c5
Johneryy <johnakintolu4@gmail.com>
2022-06-26 01:36:57 +0800

    practicals tho

---
+++ src/StaticKeyword/Main.java
@@ -0,0 +1,9 @@
+package StaticKeyword;
+
+public class Main {
+    public static void main(String[] args) {
+        Friend friend1 = new Friend("Johnery");
+        Friend friend2 = new Friend("Omotola");
+        System.out.println();
+    }
+}
