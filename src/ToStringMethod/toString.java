c0a18094d09b48b6813e71d3284176bc492cfff1
Johneryy <johnakintolu4@gmail.com>
2022-06-26 01:21:01 +0800

    practicals tho

--- src/ToStringMethod/toString.java
+++ src/ToStringMethod/toString.java
@@ -3,6 +3,8 @@ package ToStringMethod;
 public class toString {
     public static void main(String[] args) {
         Car car = new Car();
+        System.out.println(car.toString());
+        System.out.println(car);
 
     }
 }
