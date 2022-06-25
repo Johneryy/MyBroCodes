1cc3a02c427059843f6acfc50945fa2df19506f7
Johneryy <johnakintolu4@gmail.com>
2022-06-26 01:47:53 +0800

    practicals tho

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -3,9 +3,10 @@ package Inheritance;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
-        car.go();
+//        car.go();
         Bicycle bicycle = new Bicycle();
-        bicycle.speed();
+//        bicycle.speed();
+        System.out.println(bicycle.speed);
 
     }
 }
