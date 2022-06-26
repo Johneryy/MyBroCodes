958e729336a9cbe9e827f40eeb055df0d62dbdbb
Johneryy <johnakintolu4@gmail.com>
2022-06-26 23:28:11 +0800

    practicals tho

--- src/Buttons/MyFrame.java
+++ src/Buttons/MyFrame.java
@@ -6,10 +6,15 @@ import javax.swing.*;
 public class MyFrame extends JFrame {
     JButton button;
     MyFrame() {
+        ImageIcon icon = new ImageIcon("point.png");
 
         button = new JButton();
-        button.setBounds(200,100,100,50);
+        button.setBounds(100,100,100,100);
         button.addActionListener(e -> System.out.println("I created this"));
+        button.setText("Click");
+        button.setFocusable(false);
+        button.setIcon(icon);
+        button.setHorizontalTextPosition(JButton.CENTER);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
