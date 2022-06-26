ddf7e2f14fe7d7f90ca1fa7f44af594965f3f361
Johneryy <johnakintolu4@gmail.com>
2022-06-26 23:33:40 +0800

    practicals tho

--- src/Buttons/MyFrame.java
+++ src/Buttons/MyFrame.java
@@ -2,6 +2,7 @@ package Buttons;
 
 import javax.swing.JButton;
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
     JButton button;
@@ -9,12 +10,17 @@ public class MyFrame extends JFrame {
         ImageIcon icon = new ImageIcon("point.png");
 
         button = new JButton();
-        button.setBounds(100,100,100,100);
+        button.setBounds(100,100,200,100);
         button.addActionListener(e -> System.out.println("I created this"));
         button.setText("Click");
         button.setFocusable(false);
         button.setIcon(icon);
         button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setFont(new Font("Comic sans", Font.BOLD,30));
+        button.setIconTextGap(-15);
+        button.setForeground(Color.RED);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
