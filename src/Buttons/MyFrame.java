f005f4e7bc6e1d7bde456a3e3b4a4352b211d739
Johneryy <johnakintolu4@gmail.com>
2022-06-26 23:09:54 +0800

    practicals tho

--- src/Buttons/MyFrame.java
+++ src/Buttons/MyFrame.java
@@ -1,9 +1,13 @@
 package Buttons;
 
+import javax.swing.JButton;
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
     MyFrame() {
+
+        JButton button = new JButton();
+        button.setBounds(200,100,100,50);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
