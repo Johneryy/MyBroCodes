0be95167bf4fb14d7e91cdddc3c852ad86e73158
Johneryy <johnakintolu4@gmail.com>
2022-06-27 00:09:09 +0800

    practicals tho

--- src/Buttons/MyFrame.java
+++ src/Buttons/MyFrame.java
@@ -1,10 +1,8 @@
 package Buttons;
 
-import javax.swing.JButton;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class MyFrame extends JFrame implements ActionListener {
     JButton button;
