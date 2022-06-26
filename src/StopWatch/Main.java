5be6f6d7547b5a910cfa94ffd30ad097323b01e3
Johneryy <johnakintolu4@gmail.com>
2022-06-26 22:26:44 +0800

    practicals tho

--- src/StopWatch/Main.java
+++ src/StopWatch/Main.java
@@ -1,2 +1,53 @@
 package StopWatch;
 
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String,String> countries = new HashMap<String,String>();
+
+
+
+        //add a key and value
+
+        countries.put("USA","Washington DC");
+
+        countries.put("India","New Delhi");
+
+        countries.put("Russia","Moscow");
+
+        countries.put("China","Beijing");
+
+
+
+        //System.out.println(countries);
+
+        //countries.remove("USA");
+
+        //System.out.println(countries.get("Russia"));
+
+        //countries.clear();
+
+        //System.out.println(countries.size());
+
+        //countries.replace("USA", "Detroit");
+
+        //System.out.print(countries.containsKey("England"));
+
+        //System.out.print(countries.containsValue("Beijing"));
+
+
+
+        for(String i : countries.keySet()) {
+
+            System.out.print(i+"\t"+"= ");
+
+            System.out.println(countries.get(i));
+
+        }
+
+
+
+    }
+
+}
