9d0f469fce4721be0443d8dd5d6bad0f7e280567
Johneryy <johnakintolu4@gmail.com>
2022-06-26 22:58:38 +0800

    practicals tho

--- src/Buttons/MyFrame.java
+++ src/Buttons/MyFrame.java
@@ -4,7 +4,10 @@ import javax.swing.*;
 
 public class MyFrame extends JFrame {
     MyFrame() {
-
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500,500);
+        this.setVisible(true);
     }
 
 }
