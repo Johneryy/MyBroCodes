61aa2e9ff4bd131327cf7b1696a8a13cc71108e0
Johneryy <johnakintolu4@gmail.com>
2022-06-28 07:44:09 +0800

    practicals tho

---
+++ src/ScannerPractice/Main.java
@@ -0,0 +1,19 @@
+package ScannerPractice;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello "+ name);
+
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+        int year = LocalDate.now().minusYears(age).getYear();
+        System.out.println("You were born in "+year);
+    }
+}
